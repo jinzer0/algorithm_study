374ed2bc9516c0064c5b3c86fb336b4c1c28d188
Seojin03 <83911800+Seojin03@users.noreply.github.com>
2022-07-02 07:04:58 +0900

    Add files via upload

---
+++ seojin/seojin_week1_3.c
@@ -0,0 +1,29 @@
+/*
+    다이나믹 프로그래밍, 자료형
+    문제 2748번 [피보나치 수 2]
+    1. n값 입력(90보다 작거나 같은 자연수)
+    2. n번째 피보나치 수 출력
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#define max 90
+
+int main (void)
+{
+    long long list[max] = {0,}; //피보나치 수를 저장하는 배열
+    int n, i;
+    list[0] = 1; //첫번째 피보나치 수
+    list[1] = 1; //두번째 피보나치 수
+
+    //1.
+    scanf("%d", &n);
+    if (n > 90) return 0;
+
+    //2. 
+    for(i = 2; i < n; i++)
+    {
+        list[i] = list[i - 1] + list[i-2];
+    }
+    printf("%lld", list[n-1]);
+    return 0; 
+}
