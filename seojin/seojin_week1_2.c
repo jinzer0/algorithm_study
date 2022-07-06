b4d376ab463fe430da5fa553cca8fe963f384e4b
Seojin03 <83911800+Seojin03@users.noreply.github.com>
2022-07-07 07:43:00 +0900

    Add files via upload

---
+++ seojin/seojin_week1_2.c
@@ -0,0 +1,37 @@
+/*
+    문제 10988번 [팰린드롬인지 확인하기]
+    1. 단어 입력 (1이상 100이하의 길이, 알파벳 소문자로만 이루어짐)
+    2. 팰린드롬인지 확인 >> 함수 사용
+    3. 팰린드롬이면 1, 아니면 0 출력
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+#define length  100
+int palindrome(char string[length]); //팰린드롬 검사 함수(맞을 경우 1, 아닐 경우 0 반환)
+
+int main (void)
+{
+    char str[length];
+    int i;
+    scanf("%s", str); //단어 입력
+    printf("%d", palindrome(str)); //팰린드롬 검사 및 출력
+    return 0;
+}
+
+int palindrome(char string[length]) //맞을 경우 1, 아닐 경우 0 반환
+{   //가장 앞의 문자와 가장 뒤의 문자 비교 >> 거리를 좁혀가며 비교 >> 두 변수의 값이 일치하거나 front > back 인 경우 중단
+    int front, back;
+    front = 0;
+    back = strlen(string) - 1;
+    while (front < back){
+        if(string[front] == string[back]){
+            front = front + 1;
+            back = back - 1;
+        }
+        else
+            return 0; //팰린드롬이 아닌 경우
+    }
+    return 1; // 맞는 경우
+}
---
+++ seojin/seojin_week1_4.c
@@ -0,0 +1,49 @@
+/*
+    문제 2581번 [소수]
+    1. 자연수 M ,N 입력 (M <= N)
+    2. M이상 N 이하 자연수 중 소수인 것을 고른다.
+    3. 이들 소수의 합과 최솟값을 찾는다.
+    4. 소수가 없을 경우 -1 출력
+    5. 첫째 줄에 그 합을, 둘째 줄에 그 중 최솟값 출력
+*/
+#include <stdio.h>
+int isPrime(int k);//소수이면 0, 아니면 -1 반환
+
+int main (void)
+{
+    int m, n, min, sum = 0, i; //m은 n보다 작거나 같다. 
+
+    //1
+    scanf("%d", &m);
+    scanf("%d", &n);
+
+    //2, 3
+    for (i = m; i <= n; i++){
+        if(isPrime(i) == 0){ // 소수인 경우
+            if (sum == 0){ //최솟값 저장
+               min = i;
+            }
+            sum = sum + i;
+        }
+    }
+
+    //4. 소수가 없는 경우 > -1 출력
+    if (sum == 0) {
+        printf("-1");
+        return 0;
+    }
+
+    //5
+    printf("%d\n%d",sum,min);
+    return 0;
+}
+
+int isPrime(int k) //소수이면 0, 아니면 -1 반환
+{
+    if (k == 1) return -1;
+    if (k == 2) return 0;
+    for (int i = 2 ; i < k; i ++){
+        if ( k % i == 0) return -1;
+    }
+    return 0;
+}
