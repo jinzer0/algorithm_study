3edac1b993925f7c5aa31142e4f24ab8116a4125
Seojin03 <83911800+Seojin03@users.noreply.github.com>
2022-07-17 13:04:03 +0900

    Add files via upload

---
+++ seojin/seojin_week3_1.py
@@ -0,0 +1,13 @@
+#넘파이 사용
+import numpy as np
+count = 0 #이동 경우의 수 (결과값)
+input_data = input() #현재 나이트 위치
+x = int(ord(input_data[0])) - 96 #a의 아스키 코드는 97
+y = int(input_data[1])
+data = np.array([x,y]) #나이트 위치에 대한 1*2 행렬
+step = np.array([[-2,-1], [-1,-2], [1, -2], [2, -1], [2,1], [1,2], [-1,2],[-2,1]]) #이동 경우의 수에 대한 8*2 행렬
+result = data + step #행렬의 덧셈 (data는 값이 복제되어 8*2 행렬이 됨)
+for i in range (8):
+    if result[i][0] > 0 and result[i][0] < 9 and result[i][1] > 0 and result[i][1] <9: #결과가 맵을 벗어나지 않는 경우
+        count += 1
+print(count)
---
+++ seojin/seojin_week3_3.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int A, B, list, count = 1, result = 0, i; //A, B, list는 수열의 인덱스, count는 수열의 항 의미
+	scanf("%d %d", &A, &B);
+ 
+	for (list = 1; list <= B; count++){ // 수열의 첫번째 항부터 B번째 항까지 반복문 진행
+		for (i = 0; i < count; i++){ // count를 count번 더한다
+			if ((list >= A) && (list<=B)) result = result + count; //설정 범위 안의 인덱스일 경우 result에 값 저장
+			list++; //설정범위 안, 밖 모든 경우에 항의 인덱스는 증가
+		}
+	}
+	printf("%d", result);
+	return 0;
+}
